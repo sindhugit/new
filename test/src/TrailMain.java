d207f5f6e32d4042196f46d75b2852c20f93e836
tmp-sda-1123 <tmp-sda-1123@sda-05.csc.kth.se>
2017-10-19 15:20:49 +0200

    TETS

---
+++ test/src/TrailMain.java
@@ -0,0 +1,14 @@
+
+public class TrailMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Trial t= new Trial("sam",25);
+		t.getdetails();
+
+	}
+
+}
---
+++ test/src/Trial.java
@@ -0,0 +1,15 @@
+
+public class Trial {
+
+	String name;
+	int personage;
+	public Trial(String name,int age)
+	{
+		this.name= name;
+		this.personage= age;
+	}
+public void getdetails()
+{
+	System.out.println("name: "+name+"\nage:"+personage);
+}
+}
